cdc7286239414034266d107b8848abf940a87857
yair <shragacr@gmail.com>
2022-05-30 10:02:38 +0300

    starting to predict

--- class_leveling.py
+++ class_leveling.py
@@ -1,11 +1,10 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-import matplotlib as mpl
 from matplotlib.gridspec import GridSpec
 
 
-def show_classification_porportion(class_df):
-    class_type = ['Alinged', 'Flocking', 'grouped']
+def show_classification_balance(class_df):
+    class_type = ['Aligned', 'Flocking', 'Grouped']
     fig = plt.figure(figsize=(20, 15))
     gs = GridSpec(nrows=3, ncols=3)
     ax = fig.add_subplot(gs[0, 0: 3])
@@ -27,10 +26,9 @@ def show_classification_porportion(class_df):
     for i in range(2):
         class_df_tmp = class_df.copy()
         class_df_tmp.columns = [0, 1, 2]
-        first_df = class_df_tmp[i]
-        sec_df = class_df_tmp.loc[:,i+1:].columns
+        sec_df = class_df_tmp.loc[:, i + 1:].columns
         for l in sec_df:
-            ax = fig.add_subplot(gs[1, i+l-1])
+            ax = fig.add_subplot(gs[1, i + l - 1])
             df_all = \
                 pd.Series(
                     0,
@@ -60,10 +58,63 @@ def show_classification_porportion(class_df):
         ax.bar([s for s in map(str, df_all.index)], df_all + 0.02, color='green')
         ax.set_title(f'{class_type[i]}')
     plt.subplots_adjust(hspace=1)
-    plt.savefig(r"C:\Users\yair\Desktop\data_science\figs\classification_porp", ppi=100)
-    plt.show()
+    plt.savefig(r"C:\Users\yair\Desktop\data_science\figs\classification_balance", ppi=100)
+    plt.close()
+
+
+def show_balance_on_type(class_df, type):
+    class_df_one = \
+        class_df[class_df[type] == 1].drop(columns=type)
+    class_df_zero = \
+        class_df[class_df[type] == 0].drop(columns=type)
+    class_list = [class_df_zero, class_df_one]
+    fig = plt.figure(figsize=(20, 15))
+    gs = GridSpec(nrows=1, ncols=2)
+    for i in range(2):
+        ax = fig.add_subplot(gs[0, i])
+        df_all = \
+            pd.Series(
+                0,
+                index=[(j, k) for j in range(2) for k in range(2)],
+            )
+        df_all = df_all.add(
+            class_list[i].\
+            apply(lambda x: tuple(x), axis=1).\
+            value_counts(),
+            fill_value=0
+        ).sort_index()
+        ax.bar([s for s in map(str, df_all.index)], df_all + 0.03, color='blue')
+        ax.set_title(f'({tuple(class_list[i].columns)} on {type}={i})')
+    plt.savefig(
+        r"C:\Users\yair\Desktop\data_science\figs\classification_balance_on_" + f'{type}', ppi=100)
+    plt.close()
+
+
+def show_balance_on_two_type(class_df, type):
+    col_name = class_df.drop(columns=type).columns[0]
+    class_gb = class_df.groupby(type)[col_name]
+    fig = plt.figure(figsize=(20, 15))
+    gs = GridSpec(nrows=1, ncols=4)
+    i = 0
+    for name, df in class_gb:
+        ax = fig.add_subplot(gs[0, i])
+        df_all = \
+            pd.Series(
+                0,
+                index=[j for j in range(2)],
+            )
+        df_all = df_all.add(
+            df.value_counts(),
+            fill_value=0
+        ).sort_index()
+        ax.bar([0,1], df_all + 0.03, color='blue')
+        ax.set_title(f'(\'{col_name}\' on {tuple(type)}={name})')
+        i += 1
+    plt.savefig(
+        r"C:\Users\yair\Desktop\data_science\figs\classification_balance_on" + f'_{tuple(type)}', ppi=100)
+    plt.close()
 
 if __name__ == "__main__":
     df = pd.DataFrame({'a': [0, 1, 0, 0, 0, 1, 1, 1], 'b': [0, 0, 0, 0, 1, 1, 1, 0],
                        'c': [1, 1, 1, 1, 1, 0, 1, 0]})
-    show_classification_porportion(class_df=df)
+    show_classification_balance(class_df=df)
--- main.py
+++ main.py
@@ -2,8 +2,13 @@ import pandas as pd
 import numpy as np
 from matplotlib.gridspec import GridSpec
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 import plotly.express as px
 import class_leveling
+from sklearn.ensemble import RandomForestClassifier as RFC
+from sklearn.model_selection import train_test_split as tts
+from sklearn.metrics import confusion_matrix as CM
+from sklearn.neural_network import MLPRegressor as MLPR
 
 
 # mpl.use('agg')
@@ -11,10 +16,92 @@ import class_leveling
 
 def main(base_dir):
     df, class_df = get_df(base_dir)
-    class_levelilng.show_classification_porportion(class_df)
-  #  for group_num in range(6000, len(df.index)):
-        #show_movement(group_num, df, class_df)
-  #      show_scatter(group_num, df, class_df, s=3, color='red')
+    # class_leveling.show_classification_balance(class_df)
+    # class_leveling.show_balance_on_two_type(class_df, ['Aligned', 'Flocking'])
+    # class_leveling.show_balance_on_type(class_df, 'Flocking')
+    # class_leveling.show_balance_on_type(class_df, 'Grouped')
+    # show_correlation(df, class_df)
+    # pred_vel(df, class_df)
+
+    for group_num in range(15287, len(df.index)):
+        show_movement(group_num, df, class_df)
+        # show_scatter(group_num, df, class_df, s=3, color='red')
+        pass
+
+
+def pred(df, class_df):
+    class_df = class_df.apply(lambda x: str(tuple(x)), axis=1).map(
+        {key: val for val, key in enumerate(set(class_df.apply(
+            lambda x: str(tuple(x)), axis=1)))})
+    # class_df = class_df['Flocking']
+    X_train, X_test, Y_train, Y_test = tts(df, class_df, test_size=0.3)
+    model = RFC(n_estimators=50)
+    model.fit(X_train, Y_train)
+    labels = model.predict(X_test)
+    print(CM(labels, Y_test))
+    pass
+
+
+def pred_vel(df, class_df):
+    # df = pd.concat([df, class_df], axis=1)
+    df = df[list(class_df.apply(lambda x: x.all(), axis=1))]
+    # df = df[list(class_df.apply(lambda x: (x[0] == 0) and (x[1] == 1) and (x[2] == 0), axis=1))]
+    tmp = [[f'xVel{i}', f'yVel{i}'] for i in range(1, 201)]
+    class_list = []
+    for i_list in tmp:
+        class_list.extend(i_list)
+    Y = df[class_list].copy()
+    X = df.drop(class_list, axis=1)
+    for col_name in X.columns:
+        X[col_name] /= X[col_name].mean()
+    '''
+    row_even = list(
+        pd.Series(range(400)).apply(lambda x: x % 2 == 0))
+    row_odd = list(
+        pd.Series(range(400)).apply(lambda x: x % 2 == 1))
+    for i, row in Y.iterrows():
+        # + min is mammking weird things
+        Y_even = row[row_even] / (row[row_even] - row[row_even].min()).median() # + row[row_even].min()
+        Y_odd = row[row_odd] / (row[row_odd] - row[row_odd].min()).median() # + row[row_odd].min()
+        # Y_even = row[row_even] / row[row_even].quantile(0.99)
+        # Y_odd = row[row_odd] / row[row_odd].quantile(0.99)
+        Y.loc[i, row_even] = Y_even
+        Y.loc[i, row_odd] = Y_odd
+    '''
+
+    X_train, X_test, Y_train, Y_test = tts(X, Y, test_size=0.3)
+    model = MLPR(hidden_layer_sizes=(200, 200), alpha=0.001)
+    print('starting...')
+    model.fit(X_train, Y_train)
+    print(model.score(X_test, Y_test))
+    print(model.score(X_train, Y_train))
+    print(((model.predict(X_test) - Y_test).applymap(abs) > 0.5).sum().sum() / (Y_test.shape[1] * Y_test.shape[0]))
+    pass
+
+
+def show_correlation(df: pd.DataFrame, class_df):
+    row_len = df.shape[0] * 200
+    col_len = df.shape[1] / 200
+    tmp_df = pd.DataFrame(df.values.reshape(int(row_len), int(col_len)))
+    tmp_ts = tmp_df.iloc[:, 2:4].apply(
+        lambda x: np.linalg.norm(x.values),
+        axis=1)
+    class_df = class_df.apply(tuple, axis=1)
+    class_df = class_df.iloc[np.arange(len(class_df)).repeat(200)]
+    class_df.index = pd.Index([i for i in range(len(class_df))])
+    tmp_ts.index = pd.Index([i for i in range(len(tmp_ts))])
+    tmp_ts['class'] = class_df
+    new_gb = tmp_ts.groupby('class')
+    i = 0
+    fig = plt.figure(figsize=(15, 10))
+    gs = GridSpec(nrows=1, ncols=len(new_gb.groups.keys()))
+    ax = fig.add_subplot(gs[0, i])
+    for name, small_df in new_gb:
+        ax = fig.add_subplot(gs[0, i], sharey=ax)
+        ax.violinplot(list(small_df))
+        ax.set_title(name)
+        i += 1
+    plt.show()
 
 
 def get_df(base_dir):
@@ -23,6 +110,14 @@ def get_df(base_dir):
     for class_type in base_dir:
         name_class, csv_file = class_type
         tmp = pd.read_csv(csv_file, na_values=' ')
+        if tmp.isna().any().any():
+            tmp.iloc[-1, 0] = 831.14
+            tmp1 = tmp.loc[17293:].copy()
+            tmp1.index = tmp1.index + 1
+            tmp1.loc[17293] = tmp1.iloc[-1]
+            tmp1.drop(max(tmp1.index), inplace=True)
+            tmp1.sort_index(inplace=True)
+            tmp = pd.concat([tmp[:17293], tmp1])
         df, ts = tmp.iloc[:, :-1], tmp.iloc[:, -1]
         ts.name = name_class
         res.append(df)
@@ -31,13 +126,14 @@ def get_df(base_dir):
     #    if not res[i].equals(res[i-1]):
     #        raise ValueError('different dataset for same classification')
     class_df = pd.concat(class_res, axis=1)
+    class_df = class_df[['Aligned', 'Flocking', 'Grouped']]
     res = res[0]
     return res, class_df
 
 
 def get_one_scene(group_num, df, classification_df):
-    res = df.iloc[group_num]
-    classification = classification_df.iloc[group_num]
+    res = df.loc[group_num]
+    classification = classification_df.loc[group_num]
     res = pd.DataFrame(res.values.reshape(200, 12))
     # sort the boids according to their angle
     res['theta'] = res[[0, 1]].apply(lambda x: np.arctan2(x[1], x[0]), axis=1)
@@ -129,11 +225,11 @@ def show_scatter(group_num, df, class_df, s=4, color='green'):
     ax_place.set_title('N_Separation')
     scenario_tilte = f'id:{group_num}'
     fig.suptitle(scenario_tilte, color='darkgreen')
-    class_types_list = ['Aligned', 'Flocking', 'grouped']
+    class_types_list = ['Aligned', 'Flocking', 'Grouped']
     for i in range(3):
         classification_stat = classification[i] == 1
         fig.text(
-            0.404 + i*0.095, 0.93,
+            0.404 + i * 0.095, 0.93,
             f'{class_types_list[i]}={classification_stat}',
             ha='center', va='bottom',
             color='blue' if classification_stat else 'red',
